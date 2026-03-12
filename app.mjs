0b80aefa5218232e4300146107cec3d5494b9fb9
kkisaku <karelkis21@gmail.com>
2026-03-11 23:26:48 -0500

    add mongo

--- app.mjs
+++ app.mjs
@@ -1,7 +1,31 @@
 import express from 'express'
-
+import { MongoClient , ServerApiVersion} from 'mongodb';
 const app = express()
 const PORT = process.env.PORT || 3000;
+const uri ="mongodb+srv://garfield:lasagna@cluster0.taug6.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+serverApi: {
+version: ServerApiVersion.v1,
+strict: true,
+deprecationErrors: true,
+}
+});
+
+async function run() {
+try {
+// Connect the client to the server	(optional starting in v4.7)
+await client.connect();
+// Send a ping to confirm a successful connection
+await client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+// Ensures that the client will close when you finish/error
+await client.close();
+}
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Hello my name is karel im 25 years old  and I m in dev ')
 })
