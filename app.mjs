55ddf4c9b403700a3091061216a122777f596323
kkisaku <karelkis21@gmail.com>
2026-03-11 22:26:10 -0500

    start gcp

--- app.mjs
+++ app.mjs
@@ -1,11 +1,11 @@
 import express from 'express'
 
 const app = express()
-
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello my name is karel im 25 years old  and I m in dev ')
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
